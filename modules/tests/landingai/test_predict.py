5f5da4afa4f8a646d79d4939cc10539dbea57bf4
Yazhou Cao <yazhou.cao@landing.ai>
2023-05-09 14:55:37 -0700

    Fix predict; Add a manual integration test

--- modules/tests/landingai/test_predict.py
+++ modules/tests/landingai/test_predict.py
@@ -1,6 +1,16 @@
+import logging
+
+import cv2
+from landingai.predict import Predictor
 
 
 def test_predict():
-    api_key = "api_key"
-    api_secret = "api_secret"
-    # TODO: continue
+    api_key = ""
+    api_secret = ""
+    endpoint_id = ""
+    predictor = Predictor(endpoint_id, api_key, api_secret)
+    img = cv2.imread("modules/tests/data/cereal1.jpeg")
+    assert img is not None
+    res = predictor.predict(img)
+    assert res, "Result should not be empty or None"
+    logging.info(res)
