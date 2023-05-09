da67ab82741315254d80b99f1afcbb325612526a
William Westrick <william.westrick@landing.ai>
2023-05-09 13:16:28 -0700

    add a simple example for the "cosmic ray" use case

---
+++ examples/cosmic-rays.py
@@ -0,0 +1,51 @@
+import time
+import cv2
+
+from landingai import Predictor
+
+api_key = "LANDING_API_KEY"
+api_secret = "LANDING_API_SECRET"
+predictor = Predictor("fb3b4ff0-99ff-4a4c-b17c-76bbc72dcc70", api_key, api_secret)
+
+
+def run():
+    # connect to camera
+    vid = cv2.VideoCapture(0)  # change to cv2.VideoCapture("rtsp://xxxxxx") as needed
+
+    # create a window for visualization
+    cv2.namedWindow("window", cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    # loop forever until ctrl+c pressed
+    while True:
+        try:
+            # capture frame
+            _, frame = vid.read()
+
+            # run inference
+            result = predictor.predict(frame)
+
+            # overlay the predictions on the original image
+            visualized = result.visualize(frame)
+
+            # show the predictions
+            cv2.imshow("window", visualized)
+
+            # delay slightly before running again
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+            print("exiting...")
+            break
+        except Exception as err:
+            print(f"unexpected error: {err}")
+
+    # cleanup
+    # remove the visualization window
+    cv2.destroyWindow("window")
+    # close the video stream
+    vid.release()
+
+
+if __name__ == "__main__":
+    run()
