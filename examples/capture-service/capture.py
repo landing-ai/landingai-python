1e2c7a086e94490d4155f278ee1b13828853438c
Yazhou Cao <yazhou.cao@landing.ai>
2023-05-24 17:20:17 -0700

    Fix lint errors

--- examples/capture-service/capture.py
+++ examples/capture-service/capture.py
@@ -1,5 +1,9 @@
-import cv2, time
 import threading
+import time
+
+import cv2
+
+
 # openCV's default VideoCapture cannot drop frames so if the CPU is overloaded the stream will tart to lag behind realtime.
 # This class creates a treaded capture implementation that can stay up to date wit the stream and decodes frames only on demand
 class ThreadedCapture:
@@ -8,11 +12,11 @@ class ThreadedCapture:
         if not self.cap.isOpened():
             self.cap.release()
             raise Exception(f"Could not open stream ({name})")
-        # FPS = 1/X 
+        # FPS = 1/X
         # X = desired FPS
-        self.FPS = 1/self.cap.get(cv2.CAP_PROP_FPS)
+        self.FPS = 1 / self.cap.get(cv2.CAP_PROP_FPS)
         self.FPS_MS = int(self.FPS * 1000)
-        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1) # Limit buffering to 1 frames
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)  # Limit buffering to 1 frames
         self.lock = threading.Lock()
         self.t = threading.Thread(target=self._reader)
         self.t.daemon = True
@@ -26,7 +30,7 @@ class ThreadedCapture:
         while True:
             with self.lock:
                 ret = self.cap.grab()
-                time.sleep(self.FPS) # Limit acquisition speed
+                time.sleep(self.FPS)  # Limit acquisition speed
             if not ret:
                 break
 
@@ -36,13 +40,14 @@ class ThreadedCapture:
             _, frame = self.cap.retrieve()
         return _, frame
 
-if __name__ == '__main__':
-    stream_url = 'https://s78.ipcamlive.com/streams/4etgocfj23fhnhsne/stream.m3u8'
-    threaded_camera = ThreadedCapture(stream_url,2)
+
+if __name__ == "__main__":
+    stream_url = "https://s78.ipcamlive.com/streams/4etgocfj23fhnhsne/stream.m3u8"
+    threaded_camera = ThreadedCapture(stream_url, 2)
     while True:
         try:
             _, frame = threaded_camera.read()
-            cv2.imshow('frame', frame)
+            cv2.imshow("frame", frame)
             cv2.waitKey(threaded_camera.FPS_MS)
         except AttributeError:
             pass
--- examples/capture-service/run.py
+++ examples/capture-service/run.py
@@ -22,28 +22,33 @@ _LOGGER = logging.getLogger(__name__)
 _OUTPUT_FOLDER_PATH = "examples/output/capture-service"
 
 # How many frames per second should we capture and process
-_CAPTURE_INTERVAL = 1  # In milliseconds. Set to 1 if you want to capture at the max prediction rate
+_CAPTURE_INTERVAL = (
+    1  # In milliseconds. Set to 1 if you want to capture at the max prediction rate
+)
 
 # Percent of image change needed before we run inference
-_CHANGE_THRESHOLD = 2 
+_CHANGE_THRESHOLD = 2
 
 # Public Cloud & Sky detection segmentation model
-api_key         = "bu8y8czyonaip6ceov75nfnlpnr9blh"  
-api_secret      = "mdebq6hxq19fg86k3p53rwcxh16h2qudcfonl6sjrde334y2vxz4qj4wnefh05"  
-endpoint_id     = "432d58f6-6cd4-4108-a01c-2f023503d838"  
+api_key = "bu8y8czyonaip6ceov75nfnlpnr9blh"
+api_secret = "mdebq6hxq19fg86k3p53rwcxh16h2qudcfonl6sjrde334y2vxz4qj4wnefh05"
+endpoint_id = "432d58f6-6cd4-4108-a01c-2f023503d838"
 
 #
-# Below we provide some links to public cameras. Local RTSP cameras can also be used by specifying a local URL 
-# like "rtsp://172.25.101.151/ch0_0.h264". In order to find the URL for your camera, this page is a good start 
+# Below we provide some links to public cameras. Local RTSP cameras can also be used by specifying a local URL
+# like "rtsp://172.25.101.151/ch0_0.h264". In order to find the URL for your camera, this page is a good start
 # https://www.ispyconnect.com/cameras
 
 # Apple test stream
-# stream_url = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"  
+# stream_url = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"
 # Dexter Avenue AL - https://www.wsfa.com/weather/cams/
-# stream_url = "https://s78.ipcamlive.com/streams/4etrywblgzguidm6v/stream.m3u8" 
+# stream_url = "https://s78.ipcamlive.com/streams/4etrywblgzguidm6v/stream.m3u8"
 # Louisiana Department of Transportation - https://511la.org/
-# stream_url = "https://itsstreamingbr.dotd.la.gov/public/br-cam-015.streams/playlist.m3u8" 
-stream_url = "https://itsstreamingbr.dotd.la.gov/public/br-cam-110.streams/playlist.m3u8" 
+# stream_url = "https://itsstreamingbr.dotd.la.gov/public/br-cam-015.streams/playlist.m3u8"
+stream_url = (
+    "https://itsstreamingbr.dotd.la.gov/public/br-cam-110.streams/playlist.m3u8"
+)
+
 
 def stream(capture_frame=False, inference_mode=True, detect_change=True):
     """Enable camera and start streaming."""
@@ -61,15 +66,15 @@ def stream(capture_frame=False, inference_mode=True, detect_change=True):
         if not ret:
             _LOGGER.info("Can't receive frame (stream end?). Exiting ...")
             break
-        
+
         # Resize image if needed to Keep width t0 1024
-        if frame.shape[1]>1024: 
-            width = 1024 # Set constant width
-            height = int(frame.shape[0] * (1024/frame.shape[1]) ) # Keep aspect ratio
-            dim = (width, height)    
+        if frame.shape[1] > 1024:
+            width = 1024  # Set constant width
+            height = int(frame.shape[0] * (1024 / frame.shape[1]))  # Keep aspect ratio
+            dim = (width, height)
             # resize image
-            frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
- 
+            frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
         if capture_frame:
             filename = datetime.now().strftime("%Y-%m-%d_%H%M%S")
             write_succeed = cv2.imwrite(
@@ -82,19 +87,25 @@ def stream(capture_frame=False, inference_mode=True, detect_change=True):
         if detect_change:
             # Prepare image; grayscale and blur
             prepared_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            prepared_frame = cv2.GaussianBlur(src=prepared_frame, ksize=(5,5), sigmaX=0)
+            prepared_frame = cv2.GaussianBlur(
+                src=prepared_frame, ksize=(5, 5), sigmaX=0
+            )
             # Set previous frame and continue if there is None
-            if (previous_frame is None):
+            if previous_frame is None:
                 # First frame; there is no previous one yet
                 previous_frame = prepared_frame
                 continue
             # calculate difference and update previous frame
             diff_frame = cv2.absdiff(src1=previous_frame, src2=prepared_frame)
             # Only take different areas that are different enough (>20 / 255)
-            thresh_frame = cv2.threshold(src=diff_frame, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]        
-            change_percentage=100*cv2.countNonZero(thresh_frame)/(frame.shape[0]*frame.shape[1])
+            thresh_frame = cv2.threshold(
+                src=diff_frame, thresh=20, maxval=255, type=cv2.THRESH_BINARY
+            )[1]
+            change_percentage = (
+                100 * cv2.countNonZero(thresh_frame) / (frame.shape[0] * frame.shape[1])
+            )
             print(f"Image change {change_percentage:.2f}%")
-            if change_percentage>_CHANGE_THRESHOLD:
+            if change_percentage > _CHANGE_THRESHOLD:
                 previous_frame = prepared_frame
             else:
                 continue
@@ -103,7 +114,7 @@ def stream(capture_frame=False, inference_mode=True, detect_change=True):
             _LOGGER.info("Predicting...")
             start = time.time()
             predictions = predictor.predict(frame)
-            frame = overlay_predictions(predictions, image=frame) 
+            frame = overlay_predictions(predictions, image=frame)
             frame = np.array(frame)
             _LOGGER.info(f"Prediction completed in {time.time() - start:.02f} seconds")
         # Display the resulting frame
