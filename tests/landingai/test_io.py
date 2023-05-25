3935b9dcab1f19cb7fb3ceec8af1f669fc183b26
Yazhou Cao <yazhou.cao@landing.ai>
2023-05-24 17:57:48 -0700

    Test a fix

--- tests/landingai/test_io.py
+++ tests/landingai/test_io.py
@@ -34,7 +34,7 @@ def test_video_file_path(tmp_path_factory) -> str:
     tmp_dir = tmp_path_factory.mktemp("video_output")
     video_file = str(tmp_dir / "test.mp4")
     sampe_frame = np.random.randint(0, 255, (128, 128, 3), dtype=np.uint8)
-    video = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*"DIVX"), 24, (128, 128))
+    video = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*"H264"), 24, (128, 128))
     assert video is not None, "Failed to create a video writer."
     total_frames = 48
     for _ in range(total_frames):
