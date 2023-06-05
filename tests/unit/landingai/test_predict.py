1ebc0ff62cdad6a629cdcdfd94905b4d780fef2c
Yazhou Cao <yazhou.cao@landing.ai>
2023-06-05 16:16:31 -0700

    Bug fix: change the color scheme from BGR to RGB

---
+++ tests/unit/landingai/test_predict.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import responses
+from PIL import Image
+from responses.matchers import multipart_matcher
+
+from landingai.predict import Predictor
+
+
+@responses.activate
+def test_predict_matching_expected_request_body():
+    Path("tests/output").mkdir(parents=True, exist_ok=True)
+    img_path = "tests/data/images/frame6.png"
+    img = Image.open(img_path)
+    req_files = {"file": ("image.png", _read_image_as_png_bytes(img_path), "image/png")}
+    responses.post(
+        url="https://predict.app.landing.ai/inference/v1/predict?endpoint_id=8fc1bc53-c5c1-4154-8cc1-a08f2e17ba43",
+        match=[multipart_matcher(req_files)],
+        json={
+            "backbonetype": None,
+            "backbonepredictions": None,
+            "predictions": {
+                "score": 0.9951885938644409,
+                "labelIndex": 0,
+                "labelName": "Fire",
+            },
+            "type": "ClassificationPrediction",
+        },
+    )
+    predictor = Predictor(
+        "8fc1bc53-c5c1-4154-8cc1-a08f2e17ba43", "fake_key", "fake_secret"
+    )
+    predictor.predict(img)
+
+
+def _read_image_as_png_bytes(file_path: str) -> bytes:
+    import io
+    img_buffer = io.BytesIO()
+    image = Image.open(file_path)
+    image.save(img_buffer, format="PNG")
+    data = img_buffer.getvalue()
+    assert data is not None
+    return data
