c15a66d8fbd61941ab66c4db1b048c7b5360714c
cat101 <cat101@nowhere.com>
2023-06-09 20:01:06 -0300

    Abstract camera support

--- landingai/io.py
+++ landingai/io.py
@@ -101,3 +101,75 @@ def sample_images_from_video(
         frame_count += 1
     cap.release()
     return output
+
+def read_from_notebook_webcam():
+    # Define function to acquire images from the web browser
+    from IPython.display import display, Javascript
+    from base64 import b64decode
+    filename='/tmp/photo.jpg'
+    try:
+        from google.colab.output import eval_js
+        def take_photo():
+            quality=0.8
+            js = Javascript('''
+            async function takePhoto(quality) {
+                const div = document.createElement('div');
+                const capture = document.createElement('button');
+                capture.textContent = 'Capture';
+                div.appendChild(capture);
+
+                const video = document.createElement('video');
+                video.style.display = 'block';
+                const stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+                document.body.appendChild(div);
+                div.appendChild(video);
+                video.srcObject = stream;
+                await video.play();
+
+                // Resize the output to fit the video element.
+                google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+
+                // Wait for Capture to be clicked.
+                await new Promise((resolve) => capture.onclick = resolve);
+
+                const canvas = document.createElement('canvas');
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                canvas.getContext('2d').drawImage(video, 0, 0);
+                stream.getVideoTracks()[0].stop();
+                div.remove();
+                return canvas.toDataURL('image/jpeg', quality);
+            }
+            ''')
+            display(js)
+            data = eval_js('takePhoto({})'.format(quality))
+            binary = b64decode(data.split(',')[1])
+            with open(filename, 'wb') as f:
+                f.write(binary)
+                return filename
+    except:
+        # Capture image from local webcam using OpenCV
+        import cv2
+        
+        def take_photo():
+            cam = cv2.VideoCapture(0)
+            cv2.namedWindow("Press space to take photo")
+            cv2.startWindowThread()
+            while True:
+                ret, frame = cam.read()
+                if not ret:
+                    print("failed to grab frame")
+                    exit()
+                cv2.imshow("Press space to take photo", frame)
+                k = cv2.waitKey(1)
+                if k%256 == 32:
+                    # SPACE pressed
+                    cv2.imwrite(filename, frame)
+                    break
+            cam.release()
+            cv2.waitKey(1)
+            cv2.destroyAllWindows()
+            cv2.waitKey(1)
+            return filename
+    return take_photo
