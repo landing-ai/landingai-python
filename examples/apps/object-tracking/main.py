fc3c8a765711ebeeaa6096b7a0e7dda8b1cf0dff
Dillon Laird <dillonalaird@gmail.com>
2023-06-15 21:17:11 -0700

    initial commit for object tracking

---
+++ examples/apps/object-tracking/main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+
+st.title("Landing AI Traffic Tracking")
+st.write(
+    "This application will grab the latest 10 second traffic clip of the Pacific Coast Highway"
+    "going to Malibu and count the number of cars going northbound, southbound and parked."
+)
+st.write("Please enter you LandingLens API credentials and CloudInference Endpoint ID")
+api_key = st.text_input(
+    "LandingLens API Key", value=st.session_state.get("api_key", "")
+)
+api_secret = st.text_input(
+    "LandingLens API Secret",
+    type="password",
+    value=st.session_state.get("api_secret", ""),
+)
+endpoint_id = st.text_input(
+    "CloudInference Endpoint ID",
+    value=st.session_state.get("endpoint_id", ""),
+)
+
+
+def save(api_key, api_secret, endpoint_id):
+    st.session_state["api_key"] = api_key
+    st.session_state["api_secret"] = api_secret
+    st.session_state["endpoint_id"] = endpoint_id
+
+
+st.button("Save", on_click=save(api_key, api_secret, endpoint_id))
