9e11422b8464b1882634eba7f121c76d75135fb3
Asia <92344512+AsiaCao@users.noreply.github.com>
2023-07-06 19:40:23 -0700

    feat(data_management): support loading API key from env vars or .env file (#75)
    * feat(data_management): support loading API credentials from env vars or .env files for Media and Metadata clients
    
    * Update api docs
    
    * Update docs

--- tests/unit/landingai/data_management/test_metadata.py
+++ tests/unit/landingai/data_management/test_metadata.py
@@ -11,7 +11,7 @@ def test_set_metadata_for_single_media():
     responses._add_from_file(
         file_path="tests/data/responses/v1_set_metadata_single_media.yaml"
     )
-    metadata = Metadata(_API_KEY, _PROJECT_ID)
+    metadata = Metadata(_PROJECT_ID, _API_KEY)
     resp = metadata.update(10300467, split="train")
     assert resp["project_id"] == _PROJECT_ID
     assert len(resp["media_ids"]) == 1
@@ -26,7 +26,7 @@ def test_set_metadata_for_multiple_media():
         file_path="tests/data/responses/v1_set_metadata_multiple_medias.yaml"
     )
     media_ids = [10300467, 10300466, 10300465]
-    metadata = Metadata(_API_KEY, _PROJECT_ID)
+    metadata = Metadata(_PROJECT_ID, _API_KEY)
     resp = metadata.update(media_ids, creator="tom")
     assert resp["project_id"] == _PROJECT_ID
     assert resp["media_ids"] == media_ids
