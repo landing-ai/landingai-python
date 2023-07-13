968c7196047bbee7bec7a72b1002dff860011acb
Dillon Laird <dillonalaird@gmail.com>
2023-07-13 10:22:06 -0700

    Surfer Counter App (#79)
    * surfer counter app
    
    * lint
    
    * Update run_inference.py
    
    Signed-off-by: cat101 <matias@landing.ai>
    
    ---------
    
    Signed-off-by: cat101 <matias@landing.ai>
    Co-authored-by: cat101 <matias@landing.ai>

---
+++ examples/apps/surfer-count/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+st.title("Topanga Beach Surfer Counter")
+st.write(
+    "This application will grab the latest 10s clip of surfers from the Topanga Beach surf cam"
+    "and count the number of surfers there."
+)
+st.write("Please enter your LandingLens API key and Cloud Inference Endpoint ID.")
+api_key = st.text_input(
+    "LandingLens API Key", value=st.session_state.get("api_key", "")
+)
+endpoint_id = st.text_input(
+    "Cloud Inference Endpoint ID",
+    value=st.session_state.get("endpoint_id", ""),
+)
+
+
+def save(api_key: str, endpoint_id: str):
+    st.session_state["api_key"] = api_key
+    st.session_state["endpoint_id"] = endpoint_id
+
+
+st.button("Save", on_click=save(api_key, endpoint_id))
---
+++ examples/apps/surfer-count/pages/run_inference.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+from pathlib import Path
+from landingai.predict import Predictor
+from landingai.pipeline.image_source import NetworkedCamera, FrameSet
+
+VIDEO_CACHE_PATH = Path("cached_data")
+VIDEO_CACHE_PATH.mkdir(exist_ok=True, parents=True)
+VIDEO_CACHE_PATH = VIDEO_CACHE_PATH / "latest.mp4"
+VIDEO_LEN_SEC = 10
+FPS = 2
+PLAYLIST_URL = (
+    "https://live.hdontap.com/hls/hosb1/topanga_swellmagnet.stream/playlist.m3u8"
+)
+
+
+def get_latest_surfer_count():
+    vid_src = NetworkedCamera(PLAYLIST_URL, fps=FPS)
+    surfer_model = Predictor(
+        st.session_state["endpoint_id"], api_key=st.session_state["api_key"]
+    )
+
+    frs = FrameSet()
+    for i, frame in enumerate(vid_src):
+        if i >= VIDEO_LEN_SEC * FPS:
+            break
+        frs.extend(frame.run_predict(predictor=surfer_model).overlay_predictions())
+    frs.save_video(str(VIDEO_CACHE_PATH), video_fps=FPS, image_src="overlay")
+    surfers = frs.get_class_counts()["surfer"] / (VIDEO_LEN_SEC * FPS)
+    st.video(open(VIDEO_CACHE_PATH, "rb").read())
+    st.write(f"Surfer count: **{surfers}**")
+
+
+st.title("Surfer Counter")
+button = st.button("Get Topanga Beach Surfer Count", on_click=get_latest_surfer_count)
