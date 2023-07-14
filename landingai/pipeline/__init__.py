3bc6c18280d41a3dce6e6b38ab89135d99e69a5f
Asia <92344512+AsiaCao@users.noreply.github.com>
2023-07-14 05:09:37 -0700

    Minor refactor and docs update (#80)
    * Pass PIL image directly (avoid convert image from PIL to numpy)
    
    * Create notebook_utils.py
    
    * Fix mypy
    
    * Update docs
    
    * Rebase and fix conflicts
    
    * apply isort

--- landingai/pipeline/__init__.py
+++ landingai/pipeline/__init__.py
@@ -5,4 +5,5 @@ contains a source image and derivative metadata and images."""
 # Import image_source to enable IDE auto completion
 import landingai.pipeline.image_source as image_source
 import landingai.pipeline.postprocessing as postprocessing
+
 from .frameset import FrameSet
