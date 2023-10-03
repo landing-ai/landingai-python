dc02be285e65b94866c0941637c722623c3c056e
Dillon Laird <dillonalaird@gmail.com>
2023-10-02 20:26:24 -0700

    added bar chart for tracking facing vs not facing

--- examples/apps/zoom-app/app.py
+++ examples/apps/zoom-app/app.py
@@ -2,12 +2,16 @@ import streamlit as st
 
 
 st.title("")
-api_key = st.text_input(
-    "LandingLens API Key", value=st.session_state.get("api_key", "")
-)
-endpoint_id = st.text_input(
-    "Cloud Endpoint ID", value=st.session_state.get("endpoint_id", "")
-)
+# api_key = st.text_input(
+#     "LandingLens API Key", value=st.session_state.get("api_key", "")
+# )
+# endpoint_id = st.text_input(
+#     "Cloud Endpoint ID", value=st.session_state.get("endpoint_id", "")
+# )
+
+
+api_key = "land_sk_JkygHlib8SgryZUgumM6r8GWYfQqiKdE36xDzo4K85fDihpnuG"
+endpoint_id = "7e8c1f16-947f-45cd-9f5d-c5bdf8791126"
 
 
 def save(api_key: str, endpoint_id: str):
--- examples/apps/zoom-app/pages/run_inference.py
+++ examples/apps/zoom-app/pages/run_inference.py
@@ -1,12 +1,10 @@
 import numpy as np
 import streamlit as st
+import pandas as pd
+import altair as alt
 
-from sys import platform
 from landingai.pipeline.image_source import Webcam
 from landingai.predict import Predictor
-
-
-is_win = platform == "win32"
 
 
 if "api_key" in st.session_state and "endpoint_id" in st.session_state:
@@ -14,15 +12,41 @@ if "api_key" in st.session_state and "endpoint_id" in st.session_state:
         st.session_state["endpoint_id"], api_key=st.session_state["api_key"]
     )
     # video_src = NetworkedCamera(0, fps=1)
-    placeholder = st.empty()
+    image_placeholder = st.empty()
+    bar_chart_placeholder = st.empty()
 
+    pred_counts = {"Facing Camera": 0, "Facing Away": 0}
     with Webcam(fps=1) as video_src:
         for frame in video_src:
             frame.run_predict(model).overlay_predictions()
             if len(frame.frames) > 0:
                 frame_with_pred = frame.frames[-1].other_images["overlay"]
-                placeholder.empty()
-                with placeholder.container():
+                image_placeholder.empty()
+                with image_placeholder.container():
                     st.image(np.array(frame_with_pred))
+
+                if len(frame.predictions) > 0:
+                    pred = frame.predictions[-1].label_name
+                    pred_counts[pred] += 1
+                    with bar_chart_placeholder.container():
+                        data = pd.DataFrame(
+                            pred_counts.items(), columns=["label", "count"]
+                        )
+                        chart = (
+                            alt.Chart(data)
+                            .mark_bar()
+                            .encode(
+                                x="label",
+                                y="count",
+                                color=alt.Color(
+                                    "label",
+                                    scale=alt.Scale(
+                                        domain=["Facing Camera", "Facing Away"],
+                                        range=["#762172", "#FFD700"],
+                                    ),
+                                ),
+                            )
+                        ).interactive()
+                        st.altair_chart(chart, use_container_width=True)
 else:
     st.warning("Please enter your API Key and Endpoint ID in the sidebar.")
