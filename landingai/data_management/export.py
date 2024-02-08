a27758c6b74d5304a08880e20e68388a3d177813
Camilo Iral <juan.camilo@landing.ai>
2024-02-08 17:40:17 -0500

    simplify event logs export

--- landingai/data_management/export.py
+++ landingai/data_management/export.py
@@ -1,7 +1,11 @@
-from typing import Dict, Optional
+from typing import  Optional
 
 from landingai.data_management.client import EVENT_LOGS, LandingLens
-from datetime import datetime
+from datetime import datetime, timezone
+import requests
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class Exporter:
@@ -12,8 +16,8 @@ class Exporter:
     Example
     -------
     >>> client = Exporter(project_id, api_key)
-    >>> client.export_event_logs("2023-06-01 00:00:00.000", "2023-12-30 00:00:00.000")
-    >>> {'s3Path': 's3://.../1685595600-1703912400.csv', 'signedUrl': ''https://landinglens-bucket.s3....'}
+    >>> client.export_event_logs("2023-06-01", '/path/to/save/file.csv')
+    >>> # The csv file will be saved in the desired path.
 
     Parameters
     ----------
@@ -26,34 +30,37 @@ class Exporter:
     def __init__(self, project_id: int, api_key: Optional[str] = None):
         self._client = LandingLens(project_id=project_id, api_key=api_key)
 
-    def export_event_logs(self, from_timestamp: str, to_timestamp: str) -> Dict[str, str]:
+    def export_event_logs(self, from_date: str, save_path: str) -> None:
         """Exports the event logs of the organization from the given time range.
+
         Parameters
         ----------
-        from_timestamp: str
-            In UTC following the format "YYYY-MM-DD HH:MM:SS.SSS"
-        to_timestamp: str
-            In UTC following the format "YYYY-MM-DD HH:MM:SS.SSS"
+        from_date: str
+            In date following the format "YYYY-MM-DD"
+        save_path:
+            Desired path to save the csv file to: '/path/to/save/file.csv'
 
         Returns
         ----------
-        Dict[str, str]
-            A dictionary with the S3 path and signed url pointing to a downloadable csv file, that contains the event logs.
-            ```
-            # Example output
-            {
-                's3Path': 's3://.../1685595600-1703912400.csv',
-                'signedUrl': ''https://landinglens-bucket.s3....'
-            }
-            ```
+        None
+            The csv file will be saved in the desired path.
         """
-        # Check if from_timestamp and to_timestamp are in UTC format
         try:
-            datetime.strptime(from_timestamp, "%Y-%m-%d %H:%M:%S.%f")
-            datetime.strptime(to_timestamp, "%Y-%m-%d %H:%M:%S.%f")
+            datetime.strptime(from_date, "%Y-%m-%d")
         except ValueError:
-            raise ValueError("from_timestamp and to_timestamp must be in UTC format")
+            raise ValueError("from_date must be in YYYY-MM-DD format")
+        from_timestamp = datetime.strptime(f"{from_date} 00:00:00.00000", "%Y-%m-%d %H:%M:%S.%f")
+        to_timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
 
+        _LOGGER.info("Exporting event logs...")
         resp = self._client._api(EVENT_LOGS, params={"fromTimestamp": from_timestamp, "toTimestamp": to_timestamp})
-        resp_data = resp["data"]
-        return resp_data
+        signed_url = resp["data"].get("signedUrl")
+        _LOGGER.debug("Signed URL: ", signed_url)
+        self._download_file_from_signed_url(signed_url, save_path)
+        print(f"Event logs exported successfully to path: {save_path}")
+        return
+
+    def _download_file_from_signed_url(self, signed_url: str, save_path: str):
+        response = requests.get(signed_url)
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
--- tests/unit/landingai/data_management/test_export.py
+++ tests/unit/landingai/data_management/test_export.py
@@ -1,16 +1,17 @@
 import responses
 
 from landingai.data_management.export import Exporter
+from freezegun import freeze_time
+from unittest import mock
 
 
 @responses.activate
-def test_export_event_logs():
+@mock.patch("landingai.data_management.export.Exporter._download_file_from_signed_url")
+@freeze_time("2024-01-30 00:00:00.000000")  # Mock the current date and time
+def test_export_event_logs(mocked_method):
     responses._add_from_file(file_path="tests/data/responses/test_export_event_logs.yaml")
     project_id = 12345
     api_key = "land_sk_12345"
     client = Exporter(project_id, api_key)
-    res = client.export_event_logs("2021-06-01 00:00:00.000", "2024-01-30 00:00:00.000")
-    assert res == {
-        "s3Path": "s3://.../1622505600-1706572800.csv",
-        "signedUrl": "https://landinglens-bucket.s3.../1622505600-1706572800.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential...-SignedHeaders=host"
-    }
+    res = client.export_event_logs("2021-06-01", "./local/path/to/save/file.csv")
+    assert res is None
